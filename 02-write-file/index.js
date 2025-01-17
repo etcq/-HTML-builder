975a6a261f8c47f2b45d1fd0aaf61850f26b6ac8
Anton Sushnikov <resonans93@gmail.com>
2025-01-17 15:47:46 +0500

    feat: 02-write is done

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,26 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-const { stdout, stdin } = process;
+const { stdout: output, stdin: input } = process;
+const readline = require('readline');
 
 const textFile = path.resolve(__dirname, 'text.txt');
+
+let rl = readline.createInterface(input, output);
 
 fs.writeFile(textFile, '', (err) => {
   if (err) {
     throw err;
   }
-  stdout.write('Input a text, please\n');
-  stdin.on('data', (data) => {
-    const strData = data.toString();
-    console.log(strData);
-    fs.appendFile(textFile, strData, (err) => {
-      if (err) {
-        throw err;
-      }
-      stdout.write('File was updated\n');
-    });
-    process.on('SIGINT', () => {
-      console.log('\nGoodbye, my dear');
-      process.exit();
-    });
+  rl.setPrompt('Input text, please\n');
+  rl.prompt();
+  rl.on('line', (text) => {
+    rl.resume();
+    const currText = `${text} `;
+    if (text === 'exit') {
+      rl.close();
+    } else {
+      fs.appendFile(textFile, currText, (error) => {
+        if (error) {
+          throw error;
+        }
+      });
+    }
+  });
+  process.on('exit', () => {
+    console.log('\nGood Bye!');
+  });
+  process.on('SIGINT', () => {
+    process.exit();
   });
 });
