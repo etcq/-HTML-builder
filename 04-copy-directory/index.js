20bad8ad358db8d6e5e5a6e1a238f5eca39afaa8
Anton Sushnikov <resonans93@gmail.com>
2025-01-19 19:01:50 +0500

    feat: 04-copy-files is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,36 +1,77 @@
-const fs = require('fs');
+// const fs = require('fs');
 const { join } = require('node:path');
+const {
+  access,
+  readdir,
+  unlink,
+  mkdir,
+  copyFile,
+} = require('node:fs/promises');
 
-function copyDir() {
-  fs.access(join(__dirname, 'files-copy'), fs.constants.F_OK).then(
-    fs.rm(join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    }),
-  );
+async function copyDir() {
+  await access(join(__dirname, 'files-copy'))
+    .then(() => {
+      console.log('Yes');
+      readdir(join(__dirname, 'files-copy')).then((files) => {
+        files.forEach((file) => {
+          unlink(join(__dirname, 'files-copy', file));
+        });
+      });
+    })
+    .catch(() => {
+      console.log('No');
+      mkdir(join(__dirname, 'files-copy'), { recursive: true });
+    });
 
-  fs.mkdir(join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  fs.readdir(join(__dirname, 'files'), (err, files) => {
-    if (err) {
-      console.log(err);
-    }
+  await readdir(join(__dirname, 'files')).then((files) => {
     files.forEach((file) => {
-      fs.copyFile(
+      copyFile(
         join(__dirname, 'files', file),
         join(__dirname, 'files-copy', file),
-        (err) => {
-          if (err) {
-            console.log(err);
-          }
-        },
       );
     });
   });
 }
+
+// function copyDir() {
+//   fs.access(join(__dirname, 'files-copy'), (error) => {
+//     if (error) {
+//       fs.mkdir(join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     } else {
+//       fs.readdir(join(__dirname, 'files-copy'), (err, files) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         files.forEach((file) => {
+//           fs.unlink(join(__dirname, 'files-copy', file), (err) => {
+//             if (err) {
+//               console.log(err);
+//             }
+//           });
+//         });
+//       });
+//     }
+//     fs.readdir(join(__dirname, 'files'), (err, files) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       files.forEach((file) => {
+//         fs.copyFile(
+//           join(__dirname, 'files', file),
+//           join(__dirname, 'files-copy', file),
+//           (err) => {
+//             if (err) {
+//               console.log(err);
+//             }
+//           },
+//         );
+//       });
+//     });
+//   });
+// }
 
 copyDir();
